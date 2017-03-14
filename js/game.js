3edd106ee60048c99fe6ac6922310f6a42c4eb63
Lindsey Bieda <lindseyb@github.com>
2017-03-14 19:08:37 -0400

    Color cleanup and use tweening to animate the explosion

--- js/game.js
+++ js/game.js
@@ -1,8 +1,7 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'invaders', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-
     game.load.image('bullet', '../images/bullet.png');
     game.load.image('enemyBullet', '../images/enemy-bullet.png');
     game.load.image('ship', '../images/pixeltocat.png');
@@ -27,8 +26,8 @@ var stateText;
 var livingEnemies = [];
 
 function create() {
-
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.stage.backgroundColor = "#FFF";
 
     bullets = game.add.group();
     bullets.enableBody = true;
@@ -51,6 +50,7 @@ function create() {
     enemyBullets.setAll('checkWorldBounds', true);
     enemyBullets.setAll('width', 5);
     enemyBullets.setAll('height', 20);
+    enemyBullets.setAll('tint', '0x00B2EE');
 
     player = game.add.sprite(400, 500, 'ship');
     player.anchor.setTo(0.5, 0.5);
@@ -65,17 +65,16 @@ function create() {
     createAliens();
 
     scoreString = 'Score : ';
-    scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' });
+    scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#000' });
 
     lives = game.add.group();
-    game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
+    game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#000' });
 
-    stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
+    stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#000' });
     stateText.anchor.setTo(0.5, 0.5);
     stateText.visible = false;
 
-    for (var i = 0; i < 3; i++)
-    {
+    for (var i = 0; i < 3; i++) {
         var ship = lives.create(game.world.width - 80 + (16 * i), 60, 'ship');
         ship.anchor.setTo(0.5, 0.5);
         ship.alpha = 0.8;
@@ -83,29 +82,22 @@ function create() {
         ship.height = 16;
     }
 
-    //  An explosion pool
     explosions = game.add.group();
     explosions.createMultiple(30, 'kaboom');
     explosions.forEach(setupInvader, this);
 
-    //  And some controls to play the game with
     cursors = game.input.keyboard.createCursorKeys();
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
 }
 
 function createAliens () {
-
-    for (var y = 0; y < 4; y++)
-    {
-        for (var x = 0; x < 10; x++)
-        {
+    for (var y = 0; y < 4; y++) {
+        for (var x = 0; x < 10; x++) {
+            // TODO: dynamically randomly generate a new alien
             var alien = aliens.create(x * 48, y * 50, 'alien');
             alien.anchor.setTo(0.5, 0.5);
             alien.width = 16;
             alien.height = 16;
-            alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
-            alien.play('fly');
             alien.body.moves = false;
         }
     }
@@ -113,55 +105,39 @@ function createAliens () {
     aliens.x = 100;
     aliens.y = 50;
 
-    //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
     var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-
-    //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
 }
 
 function setupInvader (invader) {
-
     invader.anchor.x = 0.5;
     invader.anchor.y = 0.5;
-    invader.animations.add('kaboom');
-
+    invader.tint = '0x666666';
 }
 
 function descend() {
-
     aliens.y += 10;
-
 }
 
 function update() {
 
-    if (player.alive)
-    {
-        //  Reset the player, then check for movement keys
+    if (player.alive) {
         player.body.velocity.setTo(0, 0);
 
-        if (cursors.left.isDown)
-        {
+        if (cursors.left.isDown) {
             player.body.velocity.x = -200;
-        }
-        else if (cursors.right.isDown)
-        {
+        } else if (cursors.right.isDown) {
             player.body.velocity.x = 200;
         }
 
-        //  Firing?
-        if (fireButton.isDown)
-        {
+        if (fireButton.isDown) {
             fireBullet();
         }
 
-        if (game.time.now > firingTimer)
-        {
+        if (game.time.now > firingTimer) {
             enemyFires();
         }
 
-        //  Run collision
         game.physics.arcade.overlap(bullets, aliens, collisionHandler, null, this);
         game.physics.arcade.overlap(enemyBullets, player, enemyHitsPlayer, null, this);
     }
@@ -169,32 +145,24 @@ function update() {
 }
 
 function render() {
-
-    // for (var i = 0; i < aliens.length; i++)
-    // {
-    //     game.debug.body(aliens.children[i]);
-    // }
-
 }
 
 function collisionHandler (bullet, alien) {
-
-    //  When a bullet hits an alien we kill them both
     bullet.kill();
     alien.kill();
 
-    //  Increase the score
     score += 20;
     scoreText.text = scoreString + score;
 
-    //  And create an explosion :)
-    //  TODO: add the animation logic here
     var explosion = explosions.getFirstExists(false);
-    explosion.reset(alien.body.x, alien.body.y);
-    explosion.play('kaboom', 30, false, true);
 
-    if (aliens.countLiving() == 0)
-    {
+    explosion.reset(alien.body.x, alien.body.y);
+    explosion.scale.setTo(1,1);
+    var tween = game.add.tween(explosion.scale).to( { x: 0, y: 0 }, 500, Phaser.Easing.Exponential.In);
+    tween.onComplete.add(function(){ explosion.kill(); });
+    tween.start();
+
+    if (aliens.countLiving() == 0) {
         score += 1000;
         scoreText.text = scoreString + score;
 
@@ -209,24 +177,20 @@ function collisionHandler (bullet, alien) {
 }
 
 function enemyHitsPlayer (player,bullet) {
-
     bullet.kill();
 
     live = lives.getFirstAlive();
+    if (live) { live.kill(); }
 
-    if (live)
-    {
-        live.kill();
-    }
-
-    //  And create an explosion :)
     var explosion = explosions.getFirstExists(false);
-    explosion.reset(player.body.x, player.body.y);
-    explosion.play('kaboom', 30, false, true);
 
-    // When the player dies
-    if (lives.countLiving() < 1)
-    {
+    explosion.reset(player.body.x, player.body.y);
+    explosion.scale.setTo(1,1);
+    var tween = game.add.tween(explosion.scale).to( { x: 0, y: 0 }, 500, Phaser.Easing.Exponential.In);
+    tween.onComplete.add(function(){ explosion.kill(); });
+    tween.start();
+
+    if (lives.countLiving() < 1) {
         player.kill();
         enemyBullets.callAll('kill');
 
